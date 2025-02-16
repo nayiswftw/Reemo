c2a400dc775503a6d2ed23b98b8ffcc4123d0f6b
nayiswftw <pronaybiswas13407@gmail.com>
2025-02-16 22:30:57 +0530

    add initial backend setup with environment configuration, utility functions, enums, models, and middleware

---
+++ backend/src/index.ts
@@ -0,0 +1,55 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import session from "cookie-session";
+import { config } from "./config/app.config";
+import passport from "passport";
+import connectDatabase from "./config/database.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
+import { BadRequestException } from "./utils/appError";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        name: "session",
+        keys: [config.SESSION_SECRET],
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: config.NODE_ENV === "production",
+        httpOnly: true,
+        sameSite: "lax",
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());    
+
+app.use(
+    cors({
+        origin: config.FRONTEND_ORIGIN,
+        credentials: true,
+    })
+);
+
+app.get(
+    `/`, 
+    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+        return res.status(HTTPSTATUS.OK).json({ 
+            message: "Hello World" 
+        });
+    })
+);
+
+app.use(errorHandler);
+
+app.listen(config.PORT, async () => {
+    console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+    await connectDatabase();
+});
