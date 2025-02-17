912f6080d2e4486c0b716f62af9a1f8e762bcda6
nayiswftw <pronaybiswas13407@gmail.com>
2025-02-17 19:22:47 +0530

    include authentication and user routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,21 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import session from "cookie-session";
 import { config } from "./config/app.config";
-import passport from "passport";
 import connectDatabase from "./config/database.config";
 import { errorHandler } from "./middlewares/errorHandler.middleware";
 import { HTTPSTATUS } from "./config/http.config";
 import { asyncHandler } from "./middlewares/asyncHandler.middleware";
 import { BadRequestException } from "./utils/appError";
+import { ErrorCodeEnum } from "./enums/error-code.enum";
+
+import "./config/passport.config";
+import passport from "passport";
+import authRoutes from "./routes/auth.route";
+import userRoutes from "./routes/user.route";
+import isAuthenticated from "./middlewares/isAuthenticated.middleware";
+import workspaceRoutes from "./routes/workspace.route";
+import memberRoutes from "./routes/member.route";
+
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -18,38 +27,48 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
-    session({
-        name: "session",
-        keys: [config.SESSION_SECRET],
-        maxAge: 24 * 60 * 60 * 1000,
-        secure: config.NODE_ENV === "production",
-        httpOnly: true,
-        sameSite: "lax",
-    })
+  session({
+    name: "session",
+    keys: [config.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000,
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite: "lax",
+  })
 );
 
 app.use(passport.initialize());
-app.use(passport.session());    
+app.use(passport.session());
 
 app.use(
-    cors({
-        origin: config.FRONTEND_ORIGIN,
-        credentials: true,
-    })
+  cors({
+    origin: config.FRONTEND_ORIGIN,
+    credentials: true,
+  })
 );
 
 app.get(
-    `/`, 
-    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-        return res.status(HTTPSTATUS.OK).json({ 
-            message: "Hello World" 
-        });
-    })
+  `/`,
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    throw new BadRequestException(
+      "Invalid token",
+      ErrorCodeEnum.AUTH_INVALID_TOKEN
+    );
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Hello world",
+    });
+  })
 );
+
+app.use(`${BASE_PATH}/auth`, authRoutes);
+app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
+app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
+app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
+
 
 app.use(errorHandler);
 
 app.listen(config.PORT, async () => {
-    console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
-    await connectDatabase();
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+  await connectDatabase();
 });
