121e9daa472ade0f76b5eb14d93012091d9b9409
nayiswftw <pronaybiswas13407@gmail.com>
2025-02-17 20:28:48 +0530

    Last touch, all endpoints are done! TODO: Refractoration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,6 +17,8 @@ import userRoutes from "./routes/user.route";
 import isAuthenticated from "./middlewares/isAuthenticated.middleware";
 import workspaceRoutes from "./routes/workspace.route";
 import memberRoutes from "./routes/member.route";
+import projectRoutes from "./routes/project.route";
+import taskRoutes from "./routes/task.route";
 
 
 const app = express();
@@ -55,7 +57,7 @@ app.get(
       ErrorCodeEnum.AUTH_INVALID_TOKEN
     );
     return res.status(HTTPSTATUS.OK).json({
-      message: "Hello world",
+      message: "Never gonna give you up",
     });
   })
 );
@@ -64,7 +66,8 @@ app.use(`${BASE_PATH}/auth`, authRoutes);
 app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
 app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
 app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
-
+app.use(`${BASE_PATH}/project`, isAuthenticated, projectRoutes);
+app.use(`${BASE_PATH}/task`, isAuthenticated, taskRoutes);
 
 app.use(errorHandler);
 
