9088bf7df33b9a30ca7861f52ab1641086535226
nayiswftw <pronaybiswas13407@gmail.com>
2025-02-18 11:14:15 +0530

    Add initial client setup with routing, hooks, and environment configuration

--- backend/src/utils/bcrypt.ts
+++
@@ -1,8 +0,0 @@
-// simple function to encrypt pass and compare values
-import bcrypt from "bcrypt";
-
-export const hashValue = async (value: string, saltRounds: number = 10) =>
-  await bcrypt.hash(value, saltRounds);
-
-export const compareValue = async (value: string, hashedValue: string) =>
-  await bcrypt.compare(value, hashedValue);
